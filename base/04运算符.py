3857476225f668bfb824d75a1e7327e01181ede6
xiekongying001 <xiekongying001@ke.com>
2022-10-14 11:45:35 +0800

    循环

--- base/04运算符.py
+++ base/04运算符.py
@@ -31,5 +31,52 @@ print(m, n)
 moneys = [111, 333, 444, 555]
 m1, m2, m3, m4 = moneys  # 必须全部拿到，不能多 不能少
 print(m1, m2, m3, m4)
-m11, m22, *_ = moneys  # _来占位表示其他的数组，_ 废弃变量
-print(m11, m22, _)
+m11, m22, *_ = moneys  # _来占位表示其他的数组，_ 废弃变量代表其他 *代表多个  多个值给到变量 "_"
+
+"""
+逻辑运算
+"""
+color = "红"  # and or not
+print(not 18 > 90)  # not>and>or
+print(18 > 9 and 8 < 19)
+
+"""
+成员运算符
+"""
+print("e" in "hello")  # 是否在内部
+print("111" in ["111", "222"])
+print("key" in {"key": "value"})
+print("key" not in {"key": "value"})
+
+"""
+身份运算符
+"""
+# is 判断id 地址是否一致，值可能一样，id 不一样
+
+
+"""
+流程控制if
+"""
+if color == '红':
+    print("红色")
+
+if 18 < 8:
+    print("小于")
+else:
+    print("大于")
+
+if 18 < 9:
+    print("大")
+elif 18 > 8:
+    print("消息")
+else:
+    print("无")
+
+score = input("请输入成绩: ")
+score = int(score)
+if score < 60:
+    print("不及格")
+elif score < 80:
+    print("优秀")
+else:
+    print("牛逼")
---
+++ base/06循环控制.py
@@ -0,0 +1,56 @@
+"""
+循环控制
+"""
+
+"""
+while
+
+终止循环   break
+继续下一次循环 continue  
+
+while True:
+
+else
+    while 正常条件形式的结束，会在执行while结束后，执行， 如果while 被break 中断，则不会执行， else也是while的一个语句
+"""
+
+pwd = input("请输入密码：")
+while pwd != '123456':
+    pwd = input("重新输入密码：")
+else:
+    print("登录成功")
+
+count1 = 1
+count2 = 1
+while count1 <= 9:
+    count2 = 1
+    while count2 <= count1:
+        print(f"{count2}*{count1}={count1 * count2} ")
+        count2 += 1
+    count1 += 1
+    print("\n")
+
+"""
+for 循环
+
+for 变量名  in 可迭代对象
+
+range 范围函数
+
+for break else  和 while 一样
+"""
+
+for v in ['1', '2', '4']:
+    print(v)
+
+for k in {"k1": 111, "k2": 222}:
+    print(f"{k}")
+
+for i in range(1, 10):
+    print(i)
+
+for i in range(10):
+    print(i)  # [0,10)
+
+for i in range(1, 10, 2):  # 有步长
+    print(i)
